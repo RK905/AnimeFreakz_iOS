163f3b426888bab3ef42eba04c1a3256414e2492
Rayen Kamta <rayen.kamta@uniphyhealth.com>
2017-10-06 18:41:21 -0400

    Initial Commit

---
+++ AnimeFreakz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimeFreakz
+//
+//  Created by Rayen Kamta on 10/6/17.
+//  Copyright © 2017 Rayen Kamta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
